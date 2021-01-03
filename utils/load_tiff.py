9002290ad7538819857097f0b77095cd743e3f71
PerelSmekel <perel.shmekel@gmail.com>
2021-01-03 10:47:30 +0200

    load tiff

--- utils/load_tiff.py
+++ utils/load_tiff.py
@@ -1,9 +1,7 @@
-import pathlib
 from skimage import io
 import numpy as np
 
 
-# vid_path = str(pathlib.Path('C:/') / 'Users' / 'motar' / 'PycharmProjects' / 'wf_opticflow' / 'data' / 'ImgSeq.tif')
 def load_tiff(vid_path):
     vid = io.imread(vid_path)
     vid = np.array(vid)
