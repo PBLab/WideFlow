afbce7e53ab8021278eb0d6e8eea740eea9bc141
PerelSmekel <perel.shmekel@gmail.com>
2021-01-03 10:47:02 +0200

    augment roi data set, load extended roi list

--- utils/load_matlab_vector_field.py
+++ utils/load_matlab_vector_field.py
@@ -1,6 +1,6 @@
 import h5py
 import numpy as np
-from utils.gen_utils import add_properties_to_roi_list
+from utils.gen_utils import add_properties_to_roi_list, add_morphological_adjacent_rois_to_roi_list
 
 
 def load_matlab_OF(mat_path):
@@ -14,7 +14,7 @@ def load_matlab_OF(mat_path):
             arrays[k] = np.array(v)
 
     [t, m, n] = arrays["uvHS"].shape
-    uxy = np.swapaxes(arrays["uvHS"].view(np.double).reshape((t, m, n, 2)), 1 ,2)
+    uxy = np.swapaxes(arrays["uvHS"].view(np.double).reshape((t, m, n, 2)), 1, 2)
     return uxy
 
 
@@ -28,19 +28,19 @@ def load_allen_2d_cortex_rois(file_path):
         for key, grp in f.items():
             roi_list[key] = {}
             roi_list[key]['Index'] = int(key.split('_')[1])
-            roi_list[key]['Area'] = grp['Area'].value
-            roi_list[key]['Centroid'] = grp['Centroid'].value
-            roi_list[key]['PixelIdxList'] = grp['PixelIdxList'].value - 1  # -1 to convert from matlab to python
+            roi_list[key]['Area'] = grp['Area'][()]
+            roi_list[key]['Centroid'] = grp['Centroid'][()]
+            roi_list[key]['PixelIdxList'] = grp['PixelIdxList'][()] - 1  # -1 to convert from matlab to python
             roi_list[key]['PixelIdxList'] = roi_list[key]['PixelIdxList'][0].astype(np.int32)
 
     return roi_list
 
 
-def extend_rois_list(file_path, save_path):
+def extend_rois_list(file_path, save_path, shape, order='F'):
 
     roi_list = load_allen_2d_cortex_rois(file_path)
-    roi_list = add_properties_to_roi_list(roi_list, (297, 337), 'F')
-
+    roi_list = add_properties_to_roi_list(roi_list, shape, order)
+    # roi_list = add_morphological_adjacent_rois_to_roi_list(roi_list, shape)
 
     with h5py.File(save_path, 'w') as f:
         for key, rdict in roi_list.items():
@@ -55,19 +55,24 @@ def load_extended_rois_list(file_path):
         for key, grp in f.items():
             roi_list[key] = {}
             roi_list[key]['Index'] = int(key.split('_')[1])
-            roi_list[key]['Area'] = grp['Area'].value
-            roi_list[key]['Centroid'] = grp['Centroid'].value
-            roi_list[key]['PixelIdxList'] = grp['PixelIdxList'].value - 1  # -1 to convert from matlab to python
-            roi_list[key]['outline'] = grp['outline'].value
-            roi_list[key]['top_left_bottom_rigth'] = grp['top_left_bottom_rigth'].value
+            roi_list[key]['Area'] = grp['Area'][()]
+            roi_list[key]['Centroid'] = grp['Centroid'][()]
+            roi_list[key]['PixelIdxList'] = grp['PixelIdxList'][()] - 1  # -1 to convert from matlab to python
+            roi_list[key]['outline'] = grp['outline'][()]
+            roi_list[key]['top_left_bottom_rigth'] = grp['top_left_bottom_rigth'][()]
+            roi_list[key]['Adjacent_rois_Idx'] = grp['Adjacent_rois_Idx'][()]
 
+
+    # keep keys order as index order
+    roi_list = {k: v for k, v in sorted(roi_list.items(), key=lambda item: item[1]['Index'])}
     return roi_list
 
 
-
+#
 # import pathlib
 # file_path = str(pathlib.Path('C:/') / 'Users' / 'motar' / 'PycharmProjects' / 'WideFlow' / 'data' / 'cortex_map' / 'allen_2d_cortex_rois.h5')
 # save_path = str(pathlib.Path('C:/') / 'Users' / 'motar' / 'PycharmProjects' / 'WideFlow' / 'data' / 'cortex_map' / 'allen_2d_cortex_rois_extended.h5')
-# extend_rois_list(file_path, save_path)
+# shape = (297, 337)
+# extend_rois_list(file_path, save_path, shape)
 # roi_list = load_extended_rois_list(save_path)
-# z=3
+# # z=3
