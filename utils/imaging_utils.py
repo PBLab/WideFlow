ca5cfca6a6d05cdb5990ebcd9090dc64aab52d2c
PerelSmekel <perel.shmekel@gmail.com>
2021-03-03 15:55:36 +0200

    imaging modules utils

---
+++ utils/imaging_utils.py
@@ -0,0 +1,19 @@
+
+
+def load_config(path, handler=None):
+    import os
+    import json
+
+    if not os.path.exists(path):
+        raise RuntimeError('could not find path: ' + path)
+
+    if os.path.isdir(path):
+        path = os.path.join(path, 'config.json')
+
+    with open(path, 'r') as f:
+        data = f.read()
+        config = json.loads(data)
+
+    return config
+
+
