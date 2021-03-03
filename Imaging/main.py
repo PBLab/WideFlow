0a2e3dc8eeb30119b4364c1db3eacfa8f05e873f
PerelSmekel <perel.shmekel@gmail.com>
2021-03-03 15:53:35 +0200

    main imaging module

---
+++ Imaging/main.py
@@ -0,0 +1,32 @@
+from devices.PVCam import PVCamera
+from pyvcam import pvc
+
+from core.processing import Processing
+from core.metric import Metric
+
+from utils.imaging_utils import *
+from devices.parallel_port import port
+
+import pathlib
+
+
+if __name__ == "__main__":
+    imaging_config_path = str(pathlib.Path('C:/') / 'Users' / 'motar' / 'PycharmProjects' / 'WideFlow' / 'data' / 'wf_spont_crystal_skull.tif')
+    config = load_config(imaging_config_path)
+
+    pvc.init_pvcam()
+    cam = next(PVCamera.detect_camera())
+
+
+    process = Processing.get_child_from_str(config["process_config"]["method"])
+    metric = Metric.get_child_from_str(config["metric_config"]["method"])
+
+    frame_counter = 0
+    cam.start_live()
+    while True:
+        frame = cam.get_live_frame()
+        process_result = process.cal_process(frame)
+        cue = metric.calc_metric(process_result)
+
+        if cue:
+            pass
