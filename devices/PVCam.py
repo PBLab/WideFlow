64b6f4ef27346d8d583980bb934842f8e9414dd6
rotemper <motarotem@gmail.com>
2021-04-29 18:11:59 +0300

    splice plugin

--- devices/PVCam.py
+++ devices/PVCam.py
@@ -10,18 +10,19 @@ class PVCamera(Camera):
         super().__init__(exp_time, binning)
         self.attr_count = const.ATTR_COUNT
 
-    def set_splice_post_processing_attributes(self, plugin_name, plugin_features_list):
+    def set_splice_post_processing_attributes(self, plugin_name, plugin_parameters_list):
+        # search for plugin index in pp_table
         pp_index = None
         for i in range(self.attr_count):
             self.set_param(const.PARAM_PP_INDEX, i)
             if self.get_param(const.PARAM_PP_FEAT_NAME) == plugin_name:
                 pp_index = i  # pp_index is the index of the post processing from the dict cam.pp_table
                 break
-
-        if pp_index != None:
-            for feat in plugin_features_list:
-                self.set_param(const.PARAM_PP_PARAM_INDEX, feat[0])
-                self.set_param(const.PARAM_PP_PARAM, feat[1])
+        # set plugin parameters
+        if pp_index != None: # TODO: fix for new configuration file structure
+            for param in plugin_parameters_list:
+                self.set_param(const.PARAM_PP_PARAM_INDEX, param[0])
+                self.set_param(const.PARAM_PP_PARAM, param[1])
                 # param_name = self.get_param(const.PARAM_PP_PARAM_NAME)
 
 
