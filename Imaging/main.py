921032d2b13c6fa8b10c87085ec591cbedf309ad
PerelSmekel <perel.shmekel@gmail.com>
2021-05-04 15:35:10 +0300

    keep working..

--- Imaging/main.py
+++ Imaging/main.py
@@ -1,4 +1,5 @@
 from pyvcam import pvc
+from pyvcam import constants as const
 
 from devices.PVCam import PVCamera
 from devices.cupy_cuda_kernels import *
@@ -14,7 +15,7 @@ import cv2
 from time import perf_counter
 import matplotlib.pyplot as plt
 
-fourcc = cv2.VideoWriter_fourcc('t', 'i', 'f', 'f')
+
 
 if __name__ == "__main__":
 
@@ -31,10 +32,13 @@ if __name__ == "__main__":
     buffer_dff = cp.asanyarray(np.zeros((capacity, nrows, ncols)))
     buffer_th = cp.asanyarray(np.zeros((capacity, nrows, ncols)))
 
-
     # allocate preprocessing buffer in device
     d_frame_rs = cp.ndarray((nrows, ncols), dtype=cp.float32)
     d_std_map = cp.ndarray((nrows, ncols), dtype=cp.float32)
+
+    # video writer settings
+    fourcc = cv2.VideoWriter_fourcc('t', 'i', 'f', 'f')
+    writer = cv2.VideoWriter(config["acquisition_config"]["save_path"], fourcc, 25, (ncols, nrows))
 
     # open camera and set camera settings
     pvc.init_pvcam()
@@ -43,8 +47,18 @@ if __name__ == "__main__":
     if config["acquisition_config"]["splice_plugins_enable"]:
         for plugin_dict in config["acquisition_config"]["splice_plugins_settings"]:
             cam.set_splice_post_processing_attributes(plugin_dict["name"], plugin_dict["parameters"])
-
     cam.start_live()
+
+    # select rou
+    if config["camera_config"]["roi"] is None:
+        frame = cam.get_live_frame()
+        bbox = cv2.selectROI('choose roi', frame)
+        if len(bbox):
+            # convert to PyVcam format
+            bbox = [int(bbox[1]), int(bbox[1]+bbox[3]), int(bbox[0]), int(bbox[0]+bbox[2])]
+            cam.set_param(const.PARAM_ROI_COUNT, bbox)
+
+
 
     frame_counter = 0
     ptr = capacity - 1
@@ -52,11 +66,11 @@ if __name__ == "__main__":
         frame = cam.get_live_frame()
         d_frame = cp.asanyarray(frame)
 
-        resize(d_frame, d_frame_rs)
+        zoom(d_frame, d_frame_rs)
         buffer_rs[ptr, :, :] = d_frame_rs
 
     # start session
-    writer = cv2.VideoWriter(config["acquisition_config"]["save_path"], fourcc, 25, (nrows, ncols))
+
     frame_counter = 0
     ptr = capacity - 1
     while frame_counter < config["acquisition_config"]["num_of_frames"]:
@@ -70,7 +84,7 @@ if __name__ == "__main__":
         frame = cam.get_live_frame()
         d_frame = cp.asanyarray(frame)
 
-        resize(d_frame, d_frame_rs)
+        zoom(d_frame, d_frame_rs)
         buffer_rs[ptr, :, :] = d_frame_rs
 
         bs = baseline_calc_carbox(buffer_rs)
