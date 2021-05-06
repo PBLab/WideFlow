9a9008eff1059392fc4d9a32f0db0c8b35404b1f
rotemper <motarotem@gmail.com>
2021-05-06 11:28:07 +0300

    toggle selector and onselect for roi selection

--- utils/imaging_utils.py
+++ utils/imaging_utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import RectangleSelector
 
 
 def load_config(path, handler=None):
@@ -17,7 +20,19 @@ def load_config(path, handler=None):
     return config
 
 
-def crop(img, xrange, yrange):
-    return img[yrange[0]: yrange[1], xrange[0]: xrange[1]]
+def onselect(eclick, erelease):
+    "eclick and erelease are matplotlib events at press and release."
+    print('startposition: (%f, %f)' % (eclick.xdata, eclick.ydata))
+    print('endposition  : (%f, %f)' % (erelease.xdata, erelease.ydata))
+    print('used button  : ', eclick.button)
+
+def toggle_selector(event):
+    print('Key pressed.')
+    if event.key in ['Q', 'q'] and toggle_selector.RS.active:
+        print('RectangleSelector deactivated.')
+        toggle_selector.RS.set_active(False)
+    if event.key in ['A', 'a'] and not toggle_selector.RS.active:
+        print('RectangleSelector activated.')
+        toggle_selector.RS.set_active(True)
 
 
