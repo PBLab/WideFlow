b2076c57cc88cd071d827cbc1b2e2ac92c3b40ef
rotemper <motarotem@gmail.com>
2021-05-11 15:28:14 +0300

    class to control serial port

---
+++ devices/serial_port.py
@@ -0,0 +1,18 @@
+import serial
+import time
+
+
+class SerialControler(serial.Serial):
+    def __init__(self, port='/dev/ttyS0', baudrate=19200, timeout=0.0):
+        super().__init__(port, baudrate, timeout=timeout)
+
+    def sendTTL(self):
+        self.write(b'H')
+        # TODO: do more things if necessary
+
+    def blink(self):
+        for i in range(10):
+            self.write(b'H')
+            time.sleep(0.2)
+            self.write(b'L')
+            time.sleep(0.2)
