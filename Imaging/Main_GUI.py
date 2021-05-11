70bb8ae0d324321691a240ca13e9118a422d81c5
rotemper <motarotem@gmail.com>
2021-05-11 15:24:44 +0300

    nada

--- Imaging/Main_GUI.py
+++ Imaging/Main_GUI.py
@@ -2,7 +2,7 @@ import wx
 from Imaging.VideoCapture import VideoCapture
 from Imaging.gui_frames import AcquisitionConfig
 from Imaging.main import run_session
-from pubsub import pub
+from pubsub.pub import Publisher
 
 from pyvcam import pvc
 from devices.PVCam import PVCamera
@@ -93,7 +93,8 @@ class Main_GUI(wx.Frame):
         video_window.Show()
 
     def open_acquisition_configuration_window(self, event):
-        acquisition_config_window = AcquisitionConfig(parent=None, title='Session Acquisition Configurations')
+        acquisition_config_window = AcquisitionConfig(parent=None, title='Session Acquisition Configurations',
+                                                      config=self.configuration)
         acquisition_config_window.Show()
 
     def start_acquisition(self, event):
@@ -101,6 +102,7 @@ class Main_GUI(wx.Frame):
             run_session(self.config, self.cam)
         except:
             print("An exception occurred")
+            self.cam.close()
 
 import numpy as np
 class Camera:
@@ -117,11 +119,13 @@ class Camera:
     def get_frame(self):
         return np.random.random(self.sensor_size)*255
 
+    def start_live(self):
+        pass
+
 def main():
+    cam = Camera()
     # pvc.init_pvcam()
     # cam = next(PVCamera.detect_camera())
-
-    cam = Camera()
     app = wx.App()
     main_gui = Main_GUI(parent=None, title='blabla', cam=cam)
     main_gui.Show()
@@ -130,72 +134,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# class Example(wx.Frame):
-#     def __init__(self, parent, title):
-#         super(Example, self).__init__(parent, title=title)
-#
-#         self.InitUI()
-#         self.Centre()
-#
-#     def InitUI(self):
-#
-#         panel = wx.Panel(self)
-#
-#         font = wx.SystemSettings.GetFont(wx.SYS_SYSTEM_FONT)
-#
-#         font.SetPointSize(9)
-#
-#         vbox = wx.BoxSizer(wx.VERTICAL)
-#
-#         hbox1 = wx.BoxSizer(wx.HORIZONTAL)
-#         st1 = wx.StaticText(panel, label='Class Name')
-#         st1.SetFont(font)
-#         hbox1.Add(st1, flag=wx.RIGHT, border=8)
-#         tc = wx.TextCtrl(panel)
-#         hbox1.Add(tc, proportion=1)
-#         vbox.Add(hbox1, flag=wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, border=10)
-#
-#         vbox.Add((-1, 10))
-#
-#         hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-#         st2 = wx.StaticText(panel, label='Matching Classes')
-#         st2.SetFont(font)
-#         hbox2.Add(st2)
-#         vbox.Add(hbox2, flag=wx.LEFT | wx.TOP, border=10)
-#
-#         vbox.Add((-1, 10))
-#
-#         hbox3 = wx.BoxSizer(wx.HORIZONTAL)
-#         tc2 = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
-#         hbox3.Add(tc2, proportion=1, flag=wx.EXPAND)
-#         vbox.Add(hbox3, proportion=1, flag=wx.LEFT|wx.RIGHT|wx.EXPAND,
-#             border=10)
-#
-#         vbox.Add((-1, 25))
-#
-#         hbox4 = wx.BoxSizer(wx.HORIZONTAL)
-#         cb1 = wx.CheckBox(panel, label='Case Sensitive')
-#         cb1.SetFont(font)
-#         hbox4.Add(cb1)
-#         cb2 = wx.CheckBox(panel, label='Nested Classes')
-#         cb2.SetFont(font)
-#         hbox4.Add(cb2, flag=wx.LEFT, border=10)
-#         cb3 = wx.CheckBox(panel, label='Non-Project classes')
-#         cb3.SetFont(font)
-#         hbox4.Add(cb3, flag=wx.LEFT, border=10)
-#         vbox.Add(hbox4, flag=wx.LEFT, border=10)
-#
-#         vbox.Add((-1, 25))
-#
-#         hbox5 = wx.BoxSizer(wx.HORIZONTAL)
-#         btn1 = wx.Button(panel, label='Ok', size=(70, 30))
-#         hbox5.Add(btn1)
-#         btn_close = wx.Button(panel, label='Close', size=(70, 30))
-#         hbox5.Add(btn_close, flag=wx.LEFT|wx.BOTTOM, border=5)
-#         vbox.Add(hbox5, flag=wx.ALIGN_RIGHT|wx.RIGHT, border=10)
-#
-#         panel.SetSizer(vbox)
-
 
