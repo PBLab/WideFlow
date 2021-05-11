436d4b677d3afb1a978f34c7a20ad15e8855afd7
rotemper <motarotem@gmail.com>
2021-05-11 15:25:20 +0300

    wxpython frame to show live images

--- Imaging/VideoCapture.py
+++ Imaging/VideoCapture.py
@@ -7,18 +7,22 @@ class VideoCapture(wx.Frame):
     def __init__(self, cam, *args, **kwargs):
         super(VideoCapture, self).__init__(*args, **kwargs)
         self.cam = cam
+        self.cam.open()
         self.height = cam.sensor_size[0]
         self.width = cam.sensor_size[1]
         self.imageBit = wx.BitmapFromImage(wx.EmptyImage(self.height, self.width))
-        self.frame = np.zeros((self.height, self.width, 3))
+        self.frame = np.array(self.cam.get_frame(), dtype=np.float32)
+        # self.frame = np.zeros((self.height, self.width, 3))
         self.bmp = wx.Bitmap.FromBuffer(self.width, self.height, self.frame)
 
         self.InitUI()
 
-        fps = 1 / cam.exp_time
+        if cam.exp_time == 0:
+            fps = 25
+        else:
+            fps = 1 / cam.exp_time
         self.timer = wx.Timer(self)
         self.timer.Start(1. / fps)
-        # self.Bind(wx.EVT_PAINT, self.OnPaint)
 
     def InitUI(self):
 
@@ -38,7 +42,7 @@ class VideoCapture(wx.Frame):
         btn_play = wx.Button(self.panel, label='play', size=(70, 30))
         self.btn_play = btn_play
         hbox.Add(btn_play)
-        vbox.Add(hbox, flag=wx.ALIGN_LEFT | wx.ALIGN_BOTTOM, border=10)
+        vbox.Add(hbox, flag=wx.ALIGN_LEFT, border=10)
         vbox.Add((-1, 25))
 
         self.staticBit = wx.StaticBitmap(self.panel, wx.ID_ANY, self.imageBit)
@@ -47,31 +51,30 @@ class VideoCapture(wx.Frame):
         vbox.Add(hbox2)
 
         self.Bind(wx.EVT_BUTTON, self.play, btn_play)
-
-    def OnPaint(self, evt):
-        dc = wx.BufferedPaintDC(self)
-        dc.DrawBitmap(self.bmp, 0, 0)
+        self.Bind(wx.EVT_CLOSE, self.onClose)
 
     def NextFrame(self, event):
-        frame = self.cam.get_frame()
-        self.frame = np.stack((frame, frame, frame), axis=2)
+        # frame = self.cam.get_frame()
+        # self.frame = np.stack((frame, frame, frame), axis=2)
+        self.frame = np.array(self.cam.get_frame(), dtype=np.float32)
         self.bmp.CopyFromBuffer(self.frame)
         self.staticBit.SetBitmap(self.bmp)
         self.Refresh()
         print('frame!!')
 
-    def onClose(self):
+    def onClose(self, event):
         self.cam.close()
+        self.Destroy()
 
     def play(self, event):
-        self.cam.open()
+        self.cam.start_live()
         self.Bind(wx.EVT_TIMER, self.NextFrame)
 
         self.Bind(wx.EVT_BUTTON, self.pause, self.btn_play)
         self.btn_play.SetLabel('pause')
 
     def pause(self, event):
-        self.cam.close()
+        self.cam.stop_live()
         self.Unbind(wx.EVT_TIMER)
 
         self.Bind(wx.EVT_BUTTON, self.play, self.btn_play)
