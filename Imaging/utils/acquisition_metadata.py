8129c18eb78113b15bca51ee449551ad3b6c4f9f
rotemper <motarotem@gmail.com>
2021-05-11 15:29:10 +0300

    class to handle session metadata

---
+++ Imaging/utils/acquisition_metadata.py
@@ -0,0 +1,35 @@
+import time
+from utils.imaging_utils import load_config
+
+
+class AcquisitionMetaData:
+    def __init__(self, session_config_path=None, config=None):
+        self.datetime = time.localtime()
+        self.session_config_path = session_config_path
+        self.config = config or load_config(session_config_path)
+        self.write_metafile_header()
+
+    def write_frame_metadata(self, timestemp, cue):
+        self.metatext += f"timestemp:{timestemp}    cue:{cue}"
+
+    def write_metafile_header(self):
+        self.metatext = str(self.datetime.tm_year) + '/' + str(self.datetime.tm_mon) + '/' + str(self.datetime.tm_mday) + ' - ' + str(self.datetime.tm_hour) + ':' + str(self.datetime.tm_min) + ':' + str(self.datetime.tm_sec) + '\n'
+        for key, value in self.config.items():
+            self.metatext += key + '\n'
+            if isinstance(value, dict):
+                self.metatext += self.dict_to_text(value)
+            elif isinstance(value, list):
+                for d in value:
+                    self.metatext += self.dict_to_text(d)
+            else:
+                self.metatext += value
+
+        self.metatext += "\nframes metadata:\n"
+
+    def dict_to_text(self, dictionary):
+        return "{" + "\n".join("{!r}: {!r},".format(k, v) for k, v in dictionary.items()) + "}"
+
+    def save_file(self):
+        path = self.config["acquisition_config"]["meta_save_path"]
+        with open(path, 'w') as f:
+            f.write(self.metatext)
