582bc2b1b78f0eed02f97206bc2df3124d598719
PerelSmekel <perel.shmekel@gmail.com>
2021-05-20 12:37:04 +0300

    contain configuration wizard

--- Imaging/gui_frames.py
+++ Imaging/gui_frames.py
@@ -1,82 +1,111 @@
-import wx
-from pubsub.pub import Publisher
 import os
-from utils.imaging_utils import load_config
-wildcard = "config json (*.json)|*.json|" \
-           "All files (*.*)|*.*"
+import wx
 
 
-class AcquisitionConfig(wx.Dialog):
-    def __init__(self, config, *args, **kwargs):
-        super(AcquisitionConfig, self).__init__(*args, **kwargs)
-        self.configs = config
-        self.currentDirectory = os.getcwd()
-        self.InitUI()
+class ConfigurationPanel(wx.Panel):
 
-    def InitUI(self):
-        self.main_layout()
-        self.Move((0, 0))
-        self.SetSize((500, 300))
-        self.SetTitle('Session Configurations')
-        self.Centre()
+    def __init__(self, parent, config=None):
+        self.config = config
+        wx.Panel.__init__(self, parent)
 
-    def main_layout(self):
-        panel = wx.Panel(self)
-        panel.SetBackgroundColour("gray")
+        # define text controller
+        self.my_text = wx.TextCtrl(self, style=wx.TE_MULTILINE)
 
-        font = wx.SystemSettings.GetFont(wx.SYS_SYSTEM_FONT)
-        font.SetPointSize(9)
+        # define buttons
+        btn_load = wx.Button(self, label='Open JSON File')
+        btn_load.Bind(wx.EVT_BUTTON, self.onOpen)
 
-        vbox = wx.BoxSizer(wx.VERTICAL)
+        btn_save = wx.Button(self, label='Save File')
+        btn_save.Bind(wx.EVT_BUTTON, self.onSave)
 
-        txt_lbl = wx.StaticText(panel, wx.ID_ANY, "Select the Input File")
-        vbox.Add(txt_lbl)
+        btn_save_as = wx.Button(self, label='Save File As')
+        btn_save_as.Bind(wx.EVT_BUTTON, self.onSaveAs)
 
-        openFileDlgBtn = wx.Button(panel, label="Browse")
-        vbox.Add(openFileDlgBtn, 0, wx.ALL, 10)
+        btn_update = wx.Button(self, label='Update\nConfigurations')
+        btn_update.Bind(wx.EVT_BUTTON, self.onUpdateConfig)
+
+        # create panel layout
+        btn_sizer = wx.BoxSizer(wx.VERTICAL)
+        btn1 = wx.BoxSizer(wx.HORIZONTAL)
+        btn1.Add(btn_load, 0, wx.ALL|wx.CENTER, 5)
+        btn1.Add(btn_save, 0, wx.ALL|wx.CENTER, 5)
+        btn1.Add(btn_save_as, 0, wx.ALL|wx.CENTER, 5)
+        btn_sizer.Add(btn1)
+
+        btn2 = wx.BoxSizer(wx.HORIZONTAL)
+        btn2.Add(btn_update, 0, wx.ALL | wx.CENTER, 5)
+        btn_sizer.Add(btn2)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.my_text, 1, wx.ALL | wx.EXPAND)
+        sizer.Add(btn_sizer, 1, wx.ALL | wx.EXPAND)
+
+        self.SetSizer(sizer)
+
+    def onOpen(self, event):
+        wildcard = "TXT files (*.json)|*.json"
+        dialog = wx.FileDialog(self, "Open Text Files", wildcard=wildcard,
+                               style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+
+        if dialog.ShowModal() == wx.ID_CANCEL:
+            return
+
+        path = dialog.GetPath()
+        self.file_path = path
+
+        if os.path.exists(path):
+            with open(path) as fobj:
+                for line in fobj:
+                    self.my_text.WriteText(line)
+
+        self.config = self.my_text.Value
+
+    def onSaveAs(self, event):
+        with wx.FileDialog(self, "Save as", wildcard="TXT files (*.json)|*.json",
+                           style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT) as fileDialog:
+
+            if fileDialog.ShowModal() == wx.ID_CANCEL:
+                return  # the user changed their mind
+
+            # save the current contents in the file
+            pathname = fileDialog.GetPath()
+            try:
+                with open(pathname, 'w') as file:
+                    self.my_text.SaveFile(pathname)
+            except IOError:
+                wx.LogError("Cannot save current data in file '%s'." % pathname)
+            self.file_path = pathname
+
+    def onSave(self, event):
+        try:
+            with open(self.file_path, 'w') as file:
+                self.my_text.SaveFile(self.file_path)
+        except IOError:
+            wx.LogError("Cannot save current data in file '%s'." % self.file_path)
+
+    def onUpdateConfig(self, event):
+        self.config = self.my_text.Value
 
 
-        panel.SetSizer(vbox)
+class ConfigurationWizard(wx.Frame):
+
+    def __init__(self, config=None):
+        wx.Frame.__init__(self, None, title='Session Acquisition Configurations')
+        self.panel = ConfigurationPanel(self, config=config)
+        self.Bind(wx.EVT_CLOSE, self.onClose)
         self.Show()
 
-        openFileDlgBtn.Bind(wx.EVT_BUTTON, self.onOpenFile)
-        self.Bind(wx.EVT_CLOSE, self.onClose)
+    def onClose(self, event):
+        #TODO: return configuration value back to main GUI frame
+        config = self.panel.config
+        self.Close()
 
-    def onOpenFile(self, event):
-        dlg = wx.FileDialog(
-            self, message="Choose a file",
-            defaultDir=self.currentDirectory,
-            defaultFile="",
-            wildcard=wildcard,
-            style=wx.FD_OPEN | wx.FD_MULTIPLE | wx.FD_CHANGE_DIR
-        )
-        if dlg.ShowModal() == wx.ID_OK:
-            path = dlg.GetPaths()
-            self.configs = load_config(path[0])
-
-    def onSaveFile(self, event):
-        pass
-
-    def onClose(self, evt):
-        # dlg = wx.MessageDialog(self, "Do you want to update the session configurations?", "Confirm Exit",
-        #                        wx.OK | wx.CANCEL | wx.ICON_QUESTION)
-        # result = dlg.ShowModal()
-        # dlg.Destroy()
-        # if result == wx.ID_CANCEL:
-        #     event = MyCustomEvent(resultOfDialog="User Clicked CANCEL")
-        #     self.GetEventHandler().ProcessEvent(event)
-        # else:  # result == wx.ID_OK
-        #     event = MyCustomEvent(resultOfDialog="User Clicked OK")
-        #     self.GetEventHandler().ProcessEvent(event)
-        self.Destroy()
-
-
-def main():
-
-    app = wx.App()
-    main_gui = AcquisitionConfig(parent=None)
-    main_gui.Show()
-    app.MainLoop()
 
 if __name__ == '__main__':
-    main()
+    app = wx.App(False)
+    frame = ConfigurationWizard()
+    app.MainLoop()
+
+
+
+
