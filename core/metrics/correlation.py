5fb02d1a4c554702ec972131ba671473c216271a
PerelSmekel <perel.shmekel@gmail.com>
2021-06-16 12:36:05 +0300

    fix calc bugs

--- core/metrics/correlation.py
+++ core/metrics/correlation.py
@@ -11,6 +11,7 @@ class Correlation(AbstractMetric):
         self.shape = x.shape
         self.capacity = self.shape[0]
         self.size = self.shape[0] * self.shape[1] * self.shape[2]
+        self.size2d = self.shape[1] * self.shape[2]
         self.dtype = self.x.dtype
 
         self.x_mean = cp.ndarray((1,), dtype=self.dtype)
@@ -20,6 +21,7 @@ class Correlation(AbstractMetric):
         self.x_old_mean = cp.ndarray((1,), dtype=self.dtype)
 
         self.old_sample = cp.ndarray(self.shape[-2:], dtype=self.dtype)
+        self.new_sample = cp.ndarray(self.shape[-2:], dtype=self.dtype)
 
         self.result = cp.float32()
 
@@ -38,6 +40,7 @@ class Correlation(AbstractMetric):
             self.ptr = 0
         else:
             self.ptr += 1
+        self.new_sample[:] = self.x[self.ptr, :, :]
 
         self.update_mean()
         self.update_std()
@@ -46,38 +49,27 @@ class Correlation(AbstractMetric):
             cp.mean(cp.multiply(cp.roll(self.x, -self.ptr, 0) - self.x_mean, self.y - self.y_mean)),
             cp.multiply(self.x_std, self.y_std))
 
-        self.old_sample = self.x[(self.ptr + 1) % self.capacity, :, :]
+        self.old_sample[:] = self.x[(self.ptr + 1) % self.capacity, :, :]
 
     def update_mean(self):
         self.x_old_mean[:] = self.x_mean
         self.x_mean[:] = self.x_mean - \
-                         cp.sum(self.old_sample - self.x[self.ptr, :, :]) / self.size
-
-        print(f"mean: {cp.mean(self.x)}      updated_mean: {self.x_mean}")
+                         cp.sum(self.old_sample - self.new_sample) / self.size
 
     def update_std(self):
-        # self.x_std[:] = \
-        #     cp.sqrt(
-        #         cp.divide(
-        #             cp.square(cp.sum(self.x[self.ptr, :, :]) / self.size - self.x_mean) +
-        #             cp.square(cp.sum(self.old_sample) / self.size - self.x_old_mean) +
-        #             cp.multiply(self.size - 1, self.x_mean - self.x_old_mean) -
-        #             2 * self.size * cp.multiply(self.x_mean, self.x_mean - self.x_old_mean) -
-        #             cp.sum(self.x[self.ptr, :, :]) / self.size
-        #             , self.size
-        #         )
-        #         + cp.square(self.x_std)
-        #     )
-
         self.x_std[:] = \
             cp.sqrt(
                 cp.divide(
-                    2 * self.size * cp.multiply(self.x_mean - self.x_old_mean,
-                                                cp.sum(self.old_sample) / self.size - self.x_old_mean) +
-                    self.size * (self.size - 1) * cp.square(self.x_mean - self.x_old_mean)
+                    cp.sum(
+                        cp.square(self.new_sample - self.x_mean) -
+                        cp.square(self.old_sample - self.x_old_mean) +
+                        2 * (self.x_mean - self.x_old_mean) * self.new_sample
+                    )
+                    - (self.size - self.size2d) * self.x_old_mean ** 2
+                    - (self.size + self.size2d) * self.x_mean ** 2
+                    + 2 * self.size * self.x_old_mean * self.x_mean
                     , self.size
                 )
                 + cp.square(self.x_std)
             )
 
-        # print(f"std: {cp.std(self.x)}      updated_std: {self.x_std}")
