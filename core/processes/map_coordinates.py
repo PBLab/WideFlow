ab77a76dc58ac6cad9e8e95ac5ff811b00f9cb1a
rotemper <motarotem@gmail.com>
2021-06-20 16:03:48 +0300

    nada

--- core/processes/map_coordinates.py
+++ core/processes/map_coordinates.py
@@ -1,16 +1,21 @@
 from core.abstract_process import AbstractProcess
 import cupyx.scipy.ndimage as csn
 import cupy as cp
+import numpy as np
 
 
 class MapCoordinates(AbstractProcess):
     def __init__(self, src, dst, coordinates, new_shape):
         self.src = src
         self.dst = dst
-        self.coordinates = coordinates
         self.new_shape = new_shape
 
-        self.dst_flat = cp.ndarray((self.new_shape[0] * self.new_shape[1], 1))
+        if int(cp.__version__[0]) < 9:
+            coordinates = np.squeeze(coordinates)
+            self.dst_flat = cp.ndarray((self.new_shape[0] * self.new_shape[1], ))
+        else:
+            self.dst_flat = cp.ndarray((self.new_shape[0] * self.new_shape[1], 1))
+        self.coordinates = coordinates
 
     def initialize_buffers(self):
         pass
