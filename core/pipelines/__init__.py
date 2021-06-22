88e07e15c913def6a4b9904ac187ddae0c51c09c
PerelSmekel <perel.shmekel@gmail.com>
2021-06-22 16:01:05 +0300

    added new pipelines

--- core/pipelines/__init__.py
+++ core/pipelines/__init__.py
@@ -1,4 +1,5 @@
 from .patterns_detection import PatternsDetection
 from .training_pipeline import TrainingPipe
+from.hemodynamics_correction import HemoDynamicsDFF
 
-__all__ = ["PatternsDetection", "TrainingPipe"]
+__all__ = ["PatternsDetection", "TrainingPipe", "HemoDynamicsDFF"]
