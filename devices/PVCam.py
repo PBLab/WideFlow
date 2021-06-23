c92571efc6576efb4963b556fc711c78598cd5f2
PerelSmekel <perel.shmekel@gmail.com>
2021-06-23 11:41:43 +0300

    send smrt_stream structure pointer and not the structure itself

--- devices/PVCam.py
+++ devices/PVCam.py
@@ -37,5 +37,6 @@ class PVCamera(Camera):
 
         smrt_stream = const.smart_stream_type(entries, params)
 
-        self.set_param(const.PARAM_SMART_STREAM_MODE_ENABLED, True)  # TODO: check which value to enter here if any: 1, True, SMTMODE_ARBITRARY_ALL....
-        self.set_param(const.PARAM_SMART_STREAM_EXP_PARAMS, smrt_stream)
+        self.set_param(const.PARAM_SMART_STREAM_MODE_ENABLED, True)
+        # self.set_param(const.PARAM_SMART_STREAM_EXP_PARAMS, smrt_stream)
+        self.set_param(const.PARAM_SMART_STREAM_EXP_PARAMS, id(smrt_stream))
