ec59ed7a8455f6632df0a8da9908230fb5d2b7df
PerelSmekel <perel.shmekel@gmail.com>
2021-07-05 11:26:04 +0300

    metric to calculate mean of specific ROI

--- core/metrics/ROI_mean.py
+++ core/metrics/ROI_mean.py
@@ -8,13 +8,20 @@ class ROIMean(AbstractMetric):
         self.x = x
         self.roi_pixels_list = roi_pixels_list
         self.ptr = ptr
+
         self.shape = x.shape
         self.capacity = self.shape[0]
-        self.pixels_inds = np.unravel_index(self.roi_pixels_list, self.shape)
+
+        self.pixels_inds = np.unravel_index(self.roi_pixels_list, (self.shape[2], self.shape[1]))
 
     def initialize_buffers(self):
         self.ptr = self.capacity - 1
 
     def evaluate(self):
-        return cp.mean(self.x[self.ptr, self.pixels_inds[0], self.pixels_inds[1]])
+        if self.ptr == self.capacity - 1:
+            self.ptr = 0
+        else:
+            self.ptr += 1
+
+        return cp.mean(self.x[self.ptr, self.pixels_inds[1], self.pixels_inds[0]])
 
