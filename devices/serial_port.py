2affe42d7aee9c28fbf4b07826c8cccb4a511caa
rotemper <motarotem@gmail.com>
2021-07-12 12:01:40 +0300

    delay timeout

--- devices/serial_port.py
+++ devices/serial_port.py
@@ -3,7 +3,7 @@ import time
 
 
 class SerialControler(serial.Serial):
-    def __init__(self, port="/dev/ttyACM0", baudrate=9800, timeout=0.0, write_timeout=1.0):
+    def __init__(self, port="/dev/ttyACM0", baudrate=9800, timeout=0.0, write_timeout=0.0):
         super().__init__(port, baudrate, timeout=timeout)
 
     def sendTTL(self):
