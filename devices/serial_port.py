1e2c9082ba7fc132ca06b568ceaed9e098d95c57
rotemper <motarotem@gmail.com>
2021-07-15 19:41:41 +0300

    new communications methods

--- devices/serial_port.py
+++ devices/serial_port.py
@@ -1,13 +1,55 @@
 import serial
-import time
+
+
+encoding = 'utf-8'
+startMarker = 60
+endMarker = 62
 
 
 class SerialControler(serial.Serial):
-    def __init__(self, port="/dev/ttyACM0", baudrate=9800, timeout=0.0, write_timeout=0.0):
+    def __init__(self, port="/dev/ttyACM0", baudrate=9600, timeout=0.0):
         super().__init__(port, baudrate, timeout=timeout)
+        self.waitForArduino()
 
     def sendTTL(self):
-        self.write(b'H')
+        self.sendToArduino("H")
 
-    def readSerial(self):
-        return self.read()
+    def sendToArduino(self, strings):
+        strings = "<" + ','.join(strings) + ">"
+        self.write(strings.encode('utf-8'))
+
+    def recvFromArduino(self):
+        ck = ""
+        x = "z"  # any value that is not an endMarker or startMarker
+        byteCount = -1  # to allow for the fact that the last increment will be one too many
+
+        # wait for the start character
+        while ord(x) != startMarker:
+            x = self.read()
+
+        # save data until the end marker is found
+        while ord(x) != endMarker:
+            if ord(x) != startMarker:
+                ck = ck + x.decode("utf-8")  # change for Python3
+                byteCount += 1
+            x = self.read()
+
+        return ck
+
+    def waitForArduino(self):
+        '''
+        wait until the Arduino sends 'Arduino Ready' - allows time for Arduino reset
+        it also ensures that any bytes left over from a previous message are discarded
+        '''
+
+        msg = ""
+        while msg.find("Arduino is ready") == -1:
+
+            while self.inWaiting() == 0:
+                pass
+
+            msg = self.recvFromArduino()
+            if msg == "Arduino is ready":
+                print(msg + "\n")
+
+
