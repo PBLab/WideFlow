cf072ba7300f46751651237536e4b76dffabdeb3
rotemper <motarotem@gmail.com>
2021-07-15 19:41:19 +0300

    old script' look for version 3

--- devices/arduino/feedback_control/feedback_control.ino
+++ devices/arduino/feedback_control/feedback_control.ino
@@ -11,23 +11,25 @@ const int ledPin = 46; // the pin that the LED is attached to
 int incomingByte;      // a variable to read incoming serial data info
 int lickPortStat;
 
-int valveClock = 0;
+double valveClock = 0;
 int valveActivationTime = 1000;
 int ledActivationTime = 2000;
-int ledClock = 0;
-int globalClock;
+double ledClock = 0;
+double globalClock;
+
+int ledAnalogVal = 100; // control the LED illumination intensity
 
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
-  
+
   // initialize the TTL pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the valve pin as an output:
   pinMode(valvePin, OUTPUT);
   // initialize the TTL pin as an output:
   pinMode(lickPortPin, INPUT);
-  
+
   //digitalWrite(ledPin, HIGH);
 }
 
@@ -37,12 +39,11 @@ void loop() {
     // read the oldest byte in the serial buffer:
     incomingByte = Serial.read();
 
-    // if it's a capital T, set vavlePin to HIGH - open selonoid valve:
     if (incomingByte == 'H') {
       valveClock = millis();
       ledClock = millis();
       digitalWrite(valvePin, HIGH);
-      digitalWrite(ledPin, HIGH);
+      analogWrite(ledPin, ledAnalogVal);
     }
   }
 
@@ -51,17 +52,17 @@ void loop() {
   if ((valveClock > 0) && (globalClock > (valveClock + valveActivationTime))) {
     valveClock = 0;
     digitalWrite(valvePin, LOW);
-    Serial.println("closing valve");
   }
-  
+
   // if ledActivationTime passed since led has been lit - close it
   if ((ledClock > 0) && (globalClock > (ledClock + ledActivationTime))) {
     ledClock = 0;
     digitalWrite(ledPin, LOW);
-    Serial.println("closing led");
   }
 
   // check lickPort status
   lickPortStat = digitalRead(lickPortPin);
+  Serial.print(lickPortStat);
   //Serial.println(lickPortStat);
+  //Serial.write(lickPortStat);
 }
