073d985c86078179f9b4bd900a318507fd558f8a
PerelSmekel <perel.shmekel@gmail.com>
2021-07-27 12:39:39 +0300

    last tif correction

--- utils/convert_dat_to_tif.py
+++ utils/convert_dat_to_tif.py
@@ -4,11 +4,21 @@ import numpy as np
 
 def convert_dat_to_tif(path, nbytes, shape, type, nframes):
     frames_offset = nbytes * shape[0]
-    for i in range(int(np.ceil(nframes / shape[0]))):
-        with TiffWriter(path[:-4] + '_' + str(i) + '.tif') as tif:
+    last_tif_shape = (nframes % shape[0], shape[1], shape[2])
+    for i in range(int(np.floor(nframes / shape[0]))):
+         with TiffWriter(path[:-4] + '_' + str(i) + '.tif') as tif:
             fr_data = np.reshape(np.fromfile(path,
                                              dtype=np.dtype(type),
                                              count=np.prod(shape),
                                              offset=frames_offset * i),
                                  shape)
+            tif.write(fr_data, contiguous=True)
+
+    if last_tif_shape[0]:
+        with TiffWriter(path[:-4] + '_' + str(i+1) + '.tif') as tif:
+            fr_data = np.reshape(np.fromfile(path,
+                                             dtype=np.dtype(type),
+                                             count=np.prod(last_tif_shape),
+                                             offset=frames_offset * (i+1)),
+                                 last_tif_shape)
             tif.write(fr_data, contiguous=True)
