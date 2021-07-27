db662dd2487735e0a5c842cfa527c7222caf24d1
rotemper <motarotem@gmail.com>
2021-07-27 12:32:47 +0300

    roi selection fix

--- Imaging/main.py
+++ Imaging/main.py
@@ -68,7 +68,7 @@ def run_session(config, cam):
             cam.set_splice_post_processing_attributes(plugin_dict["name"], plugin_dict["parameters"])
 
     # select roi
-    # cam.binning = (1, 1)  # set no binning for ROI selection
+    cam.binning = (1, 1)  # set no binning for ROI selection
     frame = cam.get_frame()
     if not os.path.exists(config["rois_data_config"]["reference_image_path"]):
         fig, ax = plt.subplots()
@@ -79,9 +79,7 @@ def run_session(config, cam):
         bbox = toggle_selector._rect_bbox
         if np.sum(bbox) > 1:
             # convert to PyVcam format
-            bbox = (bbox[0]/cam.binning[0], bbox[1]*cam.binning[0], bbox[2]/cam.binning[1], bbox[3]*cam.binning[1])
-            bbox = (int(bbox[1]), int(bbox[1] + bbox[3]), int(bbox[0]), int(bbox[0] + bbox[2]))
-            # bbox = (bbox[0], bbox[1]*cam.binning[0], bbox[2], bbox[3]*cam.binning[1])
+            bbox = (int(bbox[0]), int(bbox[0] + bbox[2]), int(bbox[1]), int(bbox[1] + bbox[3]))
             cam.roi = bbox
 
     else:  # if a reference image exist, use
@@ -95,9 +93,9 @@ def run_session(config, cam):
         xi = xi - (corr.shape[1] - frame.shape[1])
         bbox = (yi, yi + (ref_bbox[1] - ref_bbox[0]), xi, xi + ref_bbox + (ref_bbox[3] - ref_bbox[2]))
         cam.roi = bbox
-    # cam.binning = tuple(camera_config["core_attr"]["binning"])
-    # select matching points for allen atlas alignment
+    cam.binning = tuple(camera_config["core_attr"]["binning"])
 
+    # select matching points for allen atlas alignment
     frame = cam.get_frame()
     mps = MatchingPointSelector(frame, cortex_map * np.random.random(cortex_map.shape),
                                 cortex_config["cortex_matching_point"]["match_p_src"],
