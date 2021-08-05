af942fb0659df2f0dd599a833d61a2643e3a83c2
PerelSmekel <perel.shmekel@gmail.com>
2021-08-05 13:03:42 +0300

    remove all deleted files that moved to wideflow dir

--- core/metrics/__init__.py
+++
@@ -1,4 +0,0 @@
-from .correlation import Correlation
-from .ROI_mean import ROIMean
-
-__all__ = ["Correlation", "ROIMean"]
--- utils/load_tiff.py
+++
@@ -1,9 +0,0 @@
-from skimage import io
-import numpy as np
-
-
-def load_tiff(vid_path):
-    vid = io.imread(vid_path)
-    vid = np.array(vid)
-    return vid
-
