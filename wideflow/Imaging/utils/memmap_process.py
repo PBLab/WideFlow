610a4289c075d50a35199d39ef66ebff976f2b7a
rotemper <motarotem@gmail.com>
2021-08-15 14:55:45 +0300

    nada

--- wideflow/Imaging/utils/memmap_process.py
+++ wideflow/Imaging/utils/memmap_process.py
@@ -1,4 +1,8 @@
-from multiprocessing import shared_memory
+import sys
+if int(sys.version[2]) >= 8:
+    from multiprocessing import shared_memory
+else:
+    from multiprocess import shared_memory
 import numpy as np
 
 
