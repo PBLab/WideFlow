715cb95887b5bfb161b912b19dad5ecdbd3bb2d8
PerelSmekel <perel.shmekel@gmail.com>
2021-08-19 14:45:22 +0300

    handling serial_readout unknown - '?'

--- wideflow/analysis/utils/extract_from_metadata_file.py
+++ wideflow/analysis/utils/extract_from_metadata_file.py
@@ -3,6 +3,7 @@ def extract_from_metadata_file(path):
     cue = []
     metric_result = []
     serial_readout = []
+    sr_prv = 1
     with open(path, "r") as f:
         line_txt = ""
         while line_txt != "frames metadata:\n":
@@ -12,6 +13,13 @@ def extract_from_metadata_file(path):
             timestamp.append(float(line_txt[0].split(':')[1]))
             cue.append(int(line_txt[1].split(':')[1]))
             metric_result.append(float(line_txt[2].split(':')[1]))
-            serial_readout.append(int(line_txt[3].split(':')[1]))
+            sr = line_txt[3].split(':')[1][0]
+            if sr.isnumeric():  # handle readout failure - "?"
+                sr_prv = int(sr)
+                serial_readout.append(sr_prv)
+            else:
+                serial_readout.append(sr_prv)
+
+
 
     return timestamp, cue, metric_result, serial_readout
