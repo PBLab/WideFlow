7e2c039ad104d2cec08a55b990e82a08dbc96325
rotemper <motarotem@gmail.com>
2021-08-22 17:13:21 +0300

    behavioral camera process

---
+++ wideflow/Imaging/utils/behavioral_camera_process.py
@@ -0,0 +1,22 @@
+from wideflow.devices.FLIRCam import FLIRCam
+
+
+def run_triggered_behavioral_camera(query, saving_path, **camera_config):
+    cam = FLIRCam(**camera_config)
+    cam.avi_recorder.Open(saving_path, cam.avi_recorder_options)
+    cam.start_acquisition()
+    print('starting behavioral camera acquisition')
+    while True:
+        if not query.empty():
+            q = query.get()
+            if q == 'grab':
+                cap, frame = cam.grab_frame()
+                if cap:
+                    cam.save_to_avi(frame)
+
+            elif q == 'finish':
+                break
+
+    print('ending behavioral camera acquisition')
+    cam.stop_acquisition()
+    cam.close()
