fe76466595e5f7ec99fec4aa7cc189701af20d61
PerelSmekel <perel.shmekel@gmail.com>
2021-09-05 12:10:29 +0300

    finish analysis pipeline

--- wideflow/analysis/analysis_pipeline.py
+++ wideflow/analysis/analysis_pipeline.py
@@ -1,4 +1,4 @@
-from  wideflow.analysis.core import *
+from wideflow.analysis.core import *
 from wideflow.analysis.utils import *
 from wideflow.utils.load_tiff import load_tiff
 from wideflow.utils.load_bbox import load_bbox
@@ -20,10 +20,19 @@ with h5py.File(cortex_file_path, 'r') as f:
 rois_dict_path = "C:\\Users\\motar\\PycharmProjects\\WideFlow\\data\\cortex_map\\allen_2d_cortex_rois_extended.h5"
 rois_dict = load_extended_rois_list(rois_dict_path)
 
+# analysis global parameters
+crop = False
+register = True
+dff_bs_method = "moving_avg"
+accept_transform_matching_points = False
+hemo_correct_ch = ["channel_0", "channel_1"]
+
 # load session data
-register = False
-dir_path = 'Z:/Rotem/WideFlow prj/3422/01082021/'
+dir_path = 'Z:/Rotem/WideFlow prj/3697/20210901/'
 metadata, config = load_session_metadata(dir_path)
+if os.path.exists(dir_path + 'regression_coeff_map.npy'):
+    regression_coeff_map = np.load(dir_path + "regression_coeff_map.npy")
+
 
 n_channels = config["camera_config"]["attr"]["channels"]
 dff_bs_n_frames = config["acquisition_config"]["capacity"]
@@ -53,11 +62,16 @@ for p, tif_path in enumerate(wf_video_paths):
     wf_data = load_tiff(tif_path)
     if p != 0:
         wf_data = np.concatenate((wf_data_remainder, wf_data), axis=0)
-    wf_data_remainder = wf_data[-dff_bs_n_frames:, :, :]
+    wf_data_remainder = wf_data[-dff_bs_n_frames*n_channels:, :, :]
+
+    # crop
+    if crop:
+        wf_data = crop(wf_data, ref_bbox, reference_image)
 
     # register
     if register:
-        wf_data = registration(wf_data, reference_image, ref_bbox, match_p_src, match_p_dst, cortex_map)
+        wf_data = registration(wf_data, match_p_src, match_p_dst, cortex_map, accept_transform_matching_points)
+        accept_transform_matching_points = True
     else:
         temp = np.ndarray((wf_data.shape[0], cortex_mask.shape[0], cortex_mask.shape[1]), dtype=wf_data.dtype)
         for i, frame in enumerate(wf_data):
@@ -71,17 +85,16 @@ for p, tif_path in enumerate(wf_video_paths):
     # deinterleave
     wf_data = deinterleave(wf_data, n_channels)
 
-    wf_data['channel_0'] = wf_data['channel_0'][
-        np.mean(wf_data['channel_0'], axis=(1, 2)) > 12000]  # TODO: only for strobing failure
     # dff
-    wf_data = calc_dff(wf_data, "t_prj", dff_bs_n_frames)
+    wf_data = calc_dff(wf_data, dff_bs_method, dff_bs_n_frames)
     # remove remainder
     if p != 0:
         for ch in wf_data:
             wf_data[ch] = wf_data[ch][dff_bs_n_frames:, :, :]
 
     # hemodynamics attenuation
-    hemodynamics_attenuation()
+    if regression_coeff_map is not None and hemo_correct_ch != None:
+        hemodynamics_attenuation(wf_data, regression_coeff_map, hemo_correct_ch, dff_bs_n_frames)
 
     # ROIs traces extraction
     rois_traces = extract_roi_traces(wf_data, rois_dict, cortex_mask.shape)
@@ -92,9 +105,14 @@ for p, tif_path in enumerate(wf_video_paths):
             concat_rois_traces[ch][roi] = np.concatenate((concat_rois_traces[ch][roi], trace))
 
 # save rois traces
+if not os.path.isdir(dir_path + 'analysis_results'):
+    os.mkdir(dir_path + 'analysis_results')
+results_path = dir_path + 'analysis_results/'
 print("saving results")
-with h5py.File(dir_path + 'session_ROIs_traces_Tprj.h5', 'w') as f:
+with h5py.File(results_path + 'session_ROIs_traces.h5', 'w') as f:
     for ch, ch_traces_dict in concat_rois_traces.items():
         f.create_group(ch)
         for roi, trace in ch_traces_dict.items():
             f.create_dataset(f'{ch}/{roi}', data=trace)
+
+plot_figures(results_path, metadata["cue"], metadata["serial_readout"])
