79ea093ad7638600e8f271c410a174f0fbe34a99
rotemper <motarotem@gmail.com>
2021-10-17 15:58:05 +0300

    threshold rois

---
+++ wideflow/analysis/utils/dynamic_threshold_scatter_activity.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def dynamic_threshold(traces, n_std):
+    """
+
+    Args:
+        traces: 2d numpy array, each row is a sample time series
+        n_std: float, number of standard deviation from to threshold
+
+    Returns: binary 2d numpy array, each row is a sample time series
+
+    """
+
+    mn = np.mean(traces, axis=0)
+    band = np.std(traces, axis=0) * n_std
+    th = np.ones(traces.shape) * (mn - band)
+    return traces > th
