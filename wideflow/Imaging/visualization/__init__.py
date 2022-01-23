818de40e311b621b08304461e57021832a216f5a
rotemper <motarotem@gmail.com>
2022-01-23 17:01:57 +0200

    remove __init__ files

--- wideflow/Imaging/visualization/__init__.py
+++
@@ -1,5 +0,0 @@
-from Imaging.visualization.live_video import LiveVideo
-from Imaging.visualization.multiple_trace_plot import TracePlot
-from Imaging.visualization.ROIs_trace_plot import ROIsTracePlot
-
-__all__ = ["LiveVideo", "TracePlot", "ROIsTracePlot"]
