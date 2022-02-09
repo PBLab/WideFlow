ca42d321c0ec789fe7fb0d335f6e4a83f8696047
rotemper <motarotem@gmail.com>
2022-02-09 11:11:16 +0200

    fix bbox write comma

--- wideflow/utils/write_bbox_file.py
+++ wideflow/utils/write_bbox_file.py
@@ -1,3 +1,3 @@
 def write_bbox_file(path, bbox):
     with open(path, 'w') as f:
-        f.writelines(f"x_min: {bbox[0]}, x_max: {bbox[1]}, y_min: {bbox[2]}, y_max: {bbox[3]},")
+        f.writelines(f"x_min: {bbox[0]}, x_max: {bbox[1]}, y_min: {bbox[2]}, y_max: {bbox[3]}")
