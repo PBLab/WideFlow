66040ca89e7e66fec47bc2cbc43db7ee2c7b121a
rotemper <motarotem@gmail.com>
2022-04-13 17:58:18 +0300

    remove unused file and functions

--- seq2seq/vid_embedding/files_list.py
+++
@@ -1,8 +0,0 @@
-files_list = [
-    '/data/Rotem/Wide Field/wide_field_patterns/results/A_thy1/_A_thy1_02082021_channel_1.tif',
-    '/data/Rotem/Wide Field/wide_field_patterns/results/B98_thy1/_B98_thy1_02082021_channel_1.tif',
-    '/data/Rotem/Wide Field/wide_field_patterns/results/B400_thy1/_B400_thy1_02082021_channel_1.tif',
-    '/data/Rotem/Wide Field/wide_field_patterns/results/C24_CamKII/_C24_CamKII_02142021_channel_1.tif',
-    '/data/Rotem/Wide Field/wide_field_patterns/results/C79_CamKII/_C79_CamKII_02142021_channel_1.tif',
-    '/data/Rotem/Wide Field/wide_field_patterns/results/D_thy1/_D_thy1_02082021_channel_1.tif',
-]
